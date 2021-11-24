42202b1cb3cf41774ac0e76f96361523d5e64fc3
sachinchowdary <sachinbsc.sachin@gmail.com>
2021-11-24 16:09:44 +0000

    module 3 v2

--- WishList/Data/ApplicationDbContext.cs
+++ WishList/Data/ApplicationDbContext.cs
@@ -6,7 +6,7 @@ namespace WishList.Data
     public class ApplicationDbContext: DbContext
     {
         public DbSet<Item> Items { get; set; }
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        public ApplicationDbContext(DbContextOptions options):base(options)
         {
 
         }

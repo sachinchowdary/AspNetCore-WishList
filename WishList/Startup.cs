8cb43070dca3ea18dcc17ebe2743cebe50bf0062
sachinchowdary <sachinbsc.sachin@gmail.com>
2021-11-24 15:38:47 +0000

    module 2

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -31,7 +31,7 @@ namespace WishList
                 app.UseExceptionHandler("/Home/Error");
             }
             app.UseRouting();
-            app.UseEndpoints(endpoints => { endpoints.MapDefaultControllerRoute(); });
+            app.UseEndpoints(endpoints => { endpoints.MapDefaultControllerRoute(); });           
         }
     }
 }
